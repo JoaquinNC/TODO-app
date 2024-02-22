7eaa03cf8f4003ec911f0b73a36a49148d301338
Joaquin <joaquinncabadas@gmail.com>
2024-02-22 19:26:18 -0300

    Se mejoran los estilos y se agrega alerta

--- todoController.js
+++ todoController.js
@@ -1,38 +1,39 @@
 const Item = require('./todoModel');
 
 exports.getItems = (req, res) => {
-    Item.find({})
-      .then(items => {
-          if (items.length === 0) {
-              // Insertar datos si no hay ninguno
-              const defaultItems = [
-                  { name: "Esta es una tarea de ejemplo" },
-              ];
-              return Item.insertMany(defaultItems);
-          } else {
-              return items;
-          }
-      })
-      .then(items => {
-          res.render('list', { newListItem: items });
-      })
-      .catch(err => {
-          console.error("Error:", err);
-          res.status(500).send("Error al buscar datos");
-      });
+  Item.find({})
+    .then(items => {
+        if (items.length === 0) {
+            // Insertar datos si no hay ninguno
+            const defaultItems = [
+                { name: "Esta es una tarea de ejemplo" },
+            ];
+            return Item.insertMany(defaultItems);
+        } else {
+            return items;
+        }
+    })
+    .then(items => {
+        res.render('list', { newListItem: items });
+    })
+    .catch(err => {
+        console.error("Error:", err);
+        res.status(500).send("Error al buscar datos");
+    });
 };
 
 exports.createItem = (req, res) => {
-    const newItemName = req.body.n;
-    const newItem = new Item({ name: newItemName });
-    newItem.save()
-      .then(() => {
-          res.redirect('/');
-      })
-      .catch(err => {
-          console.error("Error:", err);
-          res.status(500).send("Error al guardar el elemento");
-      });
+  const newItemName = req.body.n;
+
+  const newItem = new Item({ name: newItemName });
+  newItem.save()
+    .then(() => {
+        res.redirect('/');
+    })
+    .catch(err => {
+        console.error("Error:", err);
+        res.status(500).send("Error al guardar el elemento");
+    });
 };
 
 exports.deleteItem = (req, res) => {
